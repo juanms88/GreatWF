e4f238eca8b10e9ccb61eff378bd809ef3bb39aa
Juan Solis <juanms88@gmail.com>
2010-02-07 15:13:09 -0800

    suck it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :posts
 
-  map.resources :posts
+  map.root :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
 
