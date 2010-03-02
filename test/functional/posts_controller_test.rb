fcc0ff5b76537b110889cd32a53ddd3162d73a56
Juan Solis <juanms88@gmail.com>
2010-03-02 15:56:30 -0800

    ok

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -39,4 +39,11 @@ class PostsControllerTest < ActionController::TestCase
     put :update, :id => Post.first
     assert_redirected_to posts_url
   end
+  
+  def test_destroy
+    post = Post.first
+    delete :destroy, :id => post
+    assert_redirected_to posts_url
+    assert !Post.exists?(post.id)
+  end
 end
